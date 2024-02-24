3e8272349bf1420490cbe4069be6b7b5b05ad525
Soumyaworks <soumya.banerjee98@gmail.com>
2024-02-25 02:00:27 +0530

    Codeforces Div2

---
+++ codeforces_1925/codeforces_1925A.cpp
@@ -0,0 +1,69 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    
+    //cout<<"Hello World"<<"\n";
+    int t;
+    cin>>t;
+    while(t--){
+        int n,i,j,k;
+        cin>>n>>k;
+        string ans="";
+        for(i=0;i<k;i++){
+            ans+=(char)(97+i);
+        }
+        string fans="";
+        for(i=1;i<=n;i++){
+            fans+=ans;
+        }
+        cout<<fans<<"\n";
+
+    }
+    return 0;
+}
---
+++ codeforces_1925/codeforces_1925C.cpp
@@ -0,0 +1,90 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    
+    //cout<<"Hello World"<<"\n";
+    int t;
+    cin>>t;
+    while(t--){
+        int i,j,k,n,m;
+        cin>>n>>k>>m;
+        string s;
+        cin>>s;
+        unordered_set<char>S;
+        string str="";
+        for(i=0;i<m;i++){
+            S.insert(s[i]);
+            if(S.size()==k){
+                str+=s[i];
+                S.clear();
+            }
+            // else{
+            //     S.insert(s[i]);
+            // }
+        }
+        if(str.size()>=n){
+            cout<<"YES"<<"\n";
+        }
+        else{
+            cout<<"NO"<<"\n";
+            char ch;
+            for(char c='a';c<='z';c++){
+                if(S.count(c)==0){
+                    ch=c;
+                    break;
+                }
+            }
+            while(str.size()<n){
+                str+=ch;
+            }
+            cout<<str<<"\n";
+        }
+    }
+    return 0;
+}
