7afe63d4120b5db2a70e47de61d079e576741763
Soumyaworks <soumya.banerjee98@gmail.com>
2024-02-27 01:12:41 +0530

    Added DSU code

---
+++ competitive/dsu.cpp
@@ -0,0 +1,121 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    
+    //cout<<"Hello World"<<"\n";
+    DisjointSet ds(7);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
+    // if 3 and 7 same or not
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
+
+    ds.unionBySize(3, 7);
+
+    if (ds.findUPar(3) == ds.findUPar(7)) {
+        cout << "Same\n";
+    }
+    else cout << "Not same\n";
+    return 0;
+}
