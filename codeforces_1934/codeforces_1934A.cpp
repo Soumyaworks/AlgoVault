941b1bb663f6f5450a0c8aedfb2b342fbd8f8ba7
Soumyaworks <soumya.banerjee98@gmail.com>
2024-03-03 01:45:04 +0530

    Contest problems upsolving

--- codeforces_1934/codeforces_1934A.cpp
+++ codeforces_1934/codeforces_1934A.cpp
@@ -38,29 +38,6 @@ struct TreeNode {
     //TreeNode *root=new TreeNode();
 };
 
-
-// int minPathSum(vector<vector<int>>grid, vector<vector<int>>&a) {
-//         //int c=grid[0][0];
-//         int n=grid[0].size();
-//         int m=grid.size();
-//         // int a[m][n];
-//         a[0][0]=grid[0][0];
-//         for(int i=1;i<m;i++)
-//             a[i][0]=a[i-1][0]+grid[i][0];
-        
-//         for(int j=1;j<n;j++)
-//             a[0][j]=a[0][j-1]+grid[0][j];
-        
-//         for(int i=1;i<m;i++)
-//         {
-//             for(int j=1;j<n;j++)
-//                 a[i][j]=min(a[i-1][j],a[i][j-1])+grid[i][j];
-//         }
-//         return a[m-1][n-1];
-// }
-
-
-
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -75,30 +52,16 @@ int main()
     int t;
     cin>>t;
     while(t--){
-        int n,i,j,k;
+        int i,j,k,n,l;
         cin>>n;
-        string s1,s2;
-        cin>>s1;
-        cin>>s2;
-        vector<vector<int>>matrix(2,vector<int>(n));
-        for(i=0;i<2;i++){
-            for(j=0;j<n;j++){
-                if(i==0)
-                matrix[i][j]=s1[j]-'0';
-                else
-                matrix[i][j]=s2[j]-'0';
-            }
+        vector<int>a(n);
+        for(i=0;i<n;i++){
+            cin>>a[i];
         }
-        int m1=n-1,m2=0;
-        for(i=n-1;i>=1;i--)
-			if(matrix[0][i]==1 && matrix[1][i-1]==0) m1=i-1;
-		for(i=0;i<m1;i++)
-			if(matrix[1][i]==1 && matrix[0][i+1]==0) m2=i+1;
-		for(i=0;i<=m1;i++) cout<<matrix[0][i];
-		for(int i=m1;i<n;i++) cout<<matrix[1][i];
-		cout<<"\n";
-		cout<<m1-m2+1<<"\n";
-
+        sort(a.begin(),a.end());
+        i=0,j=n-1,k=1,l=n-2;
+        int ans=abs(a[i]-a[j])+abs(a[j]-a[k])+abs(a[k]-a[l])+abs(a[l]-a[i]);
+        cout<<ans<<"\n";
     }
     return 0;
 }
--- codeforces_1934/codeforces_1934B.cpp
+++ codeforces_1934/codeforces_1934B.cpp
@@ -39,66 +39,33 @@ struct TreeNode {
 };
 
 
-// int minPathSum(vector<vector<int>>grid, vector<vector<int>>&a) {
-//         //int c=grid[0][0];
-//         int n=grid[0].size();
-//         int m=grid.size();
-//         // int a[m][n];
-//         a[0][0]=grid[0][0];
-//         for(int i=1;i<m;i++)
-//             a[i][0]=a[i-1][0]+grid[i][0];
-        
-//         for(int j=1;j<n;j++)
-//             a[0][j]=a[0][j-1]+grid[0][j];
-        
-//         for(int i=1;i<m;i++)
-//         {
-//             for(int j=1;j<n;j++)
-//                 a[i][j]=min(a[i-1][j],a[i][j-1])+grid[i][j];
-//         }
-//         return a[m-1][n-1];
-// }
-
-
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    // vector<ll> prefix_sum = calculatePrefixSum();
     // #ifndef ONLINE_JUDGE 
     //     freopen("input.txt", "r", stdin); 
     //     freopen("output.txt", "w", stdout); 
     // #endif 
     
     //cout<<"Hello World"<<"\n";
-    int t;
+    ll t;
     cin>>t;
+    vector<int>temp={0,1,2,1,2,3,1,2,3,2,1,2,2,2,3};
     while(t--){
-        int n,i,j,k;
+        ll i,j,k,n,ans,x,s=0;
         cin>>n;
-        string s1,s2;
-        cin>>s1;
-        cin>>s2;
-        vector<vector<int>>matrix(2,vector<int>(n));
-        for(i=0;i<2;i++){
-            for(j=0;j<n;j++){
-                if(i==0)
-                matrix[i][j]=s1[j]-'0';
-                else
-                matrix[i][j]=s2[j]-'0';
-            }
-        }
-        int m1=n-1,m2=0;
-        for(i=n-1;i>=1;i--)
-			if(matrix[0][i]==1 && matrix[1][i-1]==0) m1=i-1;
-		for(i=0;i<m1;i++)
-			if(matrix[1][i]==1 && matrix[0][i+1]==0) m2=i+1;
-		for(i=0;i<=m1;i++) cout<<matrix[0][i];
-		for(int i=m1;i<n;i++) cout<<matrix[1][i];
-		cout<<"\n";
-		cout<<m1-m2+1<<"\n";
-
+        if(n<15)
+            cout<< temp[n%15]<<"\n";
+        else if(n%15==5)
+            cout<<n/15 + 1<<"\n";
+        else if(n%15==8)
+            cout<<n/15+2<<"\n";
+        else
+            cout<<n/15 + temp[n%15]<<"\n";
+        // cout << ans << "\n";
     }
     return 0;
 }
