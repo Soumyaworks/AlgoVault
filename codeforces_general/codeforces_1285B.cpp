f2b936a6da44b014550cf3a1c104c5ac1704af81
Soumya Banerjee <soumya.banerjee98@gmail.com>
2024-06-05 03:03:08 +0530

    CF Practice problem Div2

---
+++ codeforces_general/codeforces_1285B.cpp
@@ -0,0 +1,87 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+ll maxSubArraySum(vector<ll>&a, int start, int size)
+{
+    ll max_so_far = INT_MIN, max_ending_here = 0;
+
+    for (ll i = start; i < size; i++) {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    
+    //cout<<"Hello World"<<"\n";
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n,i,j,k;
+        cin>>n;
+        vector<ll>a(n);
+        ll sum=0;
+        for(i=0;i<n;i++){
+            cin>>a[i];
+            sum+=a[i];
+        }
+        ll adel = maxSubArraySum(a,0,n-1);
+        adel = max(adel, maxSubArraySum(a,1,n));
+
+        if(sum>adel)
+            cout<<"YES"<<"\n";
+        else
+            cout<<"NO"<<"\n";
+    }
+    return 0;
+}
