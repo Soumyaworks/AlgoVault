ce40215a2efee1df3511b4a67a9f60e1cdc49ea8
Soumyaworks <soumya.banerjee98@gmail.com>
2024-06-28 04:03:47 +0530

    Practice Rating 1300

---
+++ codeforces_general/codeforces_1846E1.cpp
@@ -0,0 +1,82 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    
+    //cout<<"Hello World"<<"\n";
+    int t;
+    cin>>t;
+    unordered_set<ll>S;
+    ll k=2,i=1,p=1;
+    for(;k<=1000000;k++){
+        p=k;
+        ll sum=1;
+        while(p<=1000000){
+            if(sum>=(1+k+k*k)){
+                // cout<<"number being inserted: "<< sum <<"\n";
+                S.insert(sum);
+            }
+            sum+=p;
+            p=p*k;
+        }
+    }
+    // cout<<"the set:"<<"\n";
+    // for(auto x:S)
+    //     cout<<x<<" ";
+    // cout<<"\n";
+
+    while(t--){
+        int n;
+        cin>>n;
+        if(S.count(n))
+            cout<<"YES"<<"\n";
+        else
+            cout<<"NO"<<"\n";
+    }
+    return 0;
+}
