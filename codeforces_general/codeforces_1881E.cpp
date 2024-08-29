87109d8a4a1f98e83de075624470bb39d7eeb8e4
Soumyaworks <soumya.banerjee98@gmail.com>
2024-08-29 16:16:07 +0530

    CF Practice 1500

---
+++ codeforces_general/codeforces_1881E.cpp
@@ -0,0 +1,69 @@
+//Author : Soumya Banerjee
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+#pragma GCC optimize("Ofast")
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define ld long double
+#define fi first
+#define se second
+#define mod 1000000007
+#define fill(a,x) memset(a,x,sizeof(a))
+#define indexed_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+using namespace std;
+using namespace __gnu_pbds;
+
+// Defintion for a Linked List
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    //ListNode *head=new ListNode();
+    //ListNode *ptr1,*ptr2;
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    //TreeNode *root=new TreeNode();
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // #ifndef ONLINE_JUDGE 
+    //     freopen("input.txt", "r", stdin); 
+    //     freopen("output.txt", "w", stdout); 
+    // #endif 
+    
+    //cout<<"Hello World"<<"\n";
+    ll t;
+    cin>>t;
+    while(t--){
+        ll i,j,k,n;
+        cin>>n;
+        vector<ll>a(n);
+        for(auto &x:a) cin>>x;
+        vector<ll>dp(n,INT_MAX);
+        dp[0]=1;
+        if(a[0]<n) dp[a[0]]=0;
+        for(i=1;i<n;i++){
+            dp[i]=min(dp[i-1]+1,dp[i]);
+            if(i+a[i]<n) dp[i+a[i]]=min(dp[i+a[i]],dp[i-1]);
+        }
+        cout<<dp[n-1]<<"\n";
+    }
+    return 0;
+}
